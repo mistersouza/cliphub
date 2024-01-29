ef29d9fd6e78bddc5a122284eec3c70c9fe90def
Thiago Souza <mistersouza@icloud.com>
2024-01-29 15:16:37 +0000

    Set debug to true

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -50,7 +50,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 1  # 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
