6cedc5c66a6fc1a76bf9a9f04cf05162aa7c0d8b
Thiago Souza <mistersouza@icloud.com>
2024-01-29 18:08:33 +0000

    Write sidebar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,13 @@ const App = () => {
     <Router>
       <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
         <Navbar />
-        <div className="flex gap-6 md:gap-20">
+        <div className="flex gap-6 md:gap-20 py-3">
           <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
             <Sidebar />
           </div>
-          <div className="flex flex-col flex-1 gap-10 overflow-auto h-[88vh] videos mt-4"></div>
-          <Feed />
+          <div className="flex flex-col flex-1 gap-10 overflow-auto h-[88vh] videos border border-black">
+            <Feed />
+          </div>
         </div>
       </div>
     </Router>
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,9 +1,64 @@
-import React from 'react'
+import { useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
+import { ImCancelCircle } from "react-icons/im";
+
+import Discover from "./Discover";
+import Footer from "./Footer";
+import SuggestedAccounts from "./SuggestedAccounts";
+import SignUpForm from "./auth/SignUpForm";
 
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+  const [toggleSidebar, setToggleSidebar] = useState(true);
+  const user = false;
 
-export default Sidebar
+  const link =
+    "flex items-center gap-3 p-3 justify-center font-semibold text-gray-800 rounded xl:justify-start hover:bg-gray-200";
+  const activeLink =
+    "flex items-center gap-3 p-3 justify-center font-semibold xl:justify-start hover:bg-gray-200";
+
+  return (
+    <div>
+      <div
+        className="flex justify-center py-3 xl:hidden"
+        onClick={() => setToggleSidebar((prev) => !prev)}
+      >
+        {toggleSidebar ? (
+          <ImCancelCircle className="text-2xl" />
+        ) : (
+          <AiOutlineMenu className="text-2xl" />
+        )}
+      </div>
+      {toggleSidebar && (
+        <div className="flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:w-full  xl:border-0 p-3">
+          <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+            <Link to="/">
+              <div className={link}>
+                <AiFillHome className="text-2xl" />
+                <span className="hidden xl:block text-xl">For you</span>
+              </div>
+            </Link>
+          </div>
+          {!user && (
+            <div className='hidden py-5 xl:block'>
+              <p className="text-gray-500 p-2">
+                Log in to like and comment on clips
+              </p>
+              <Link
+                className="block text-center text-lg text-gray-800 font-semibold py-2.5 border border-gray-800 rounded hover:bg-gray-800 hover:text-gray-50"
+                onClick={() => {}}
+              >Log in</Link>
+            </div>
+          )}
+          <SignUpForm />
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
