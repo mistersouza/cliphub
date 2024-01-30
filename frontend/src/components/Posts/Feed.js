104dfcead6527a9d8d5be7032b21b9b116018c2b
Thiago Souza <mistersouza@icloud.com>
2024-01-30 19:09:44 +0000

    Write feed component

--- frontend/src/components/Posts/Feed.js
+++ frontend/src/components/Posts/Feed.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useState, useEffect, useContext } from "react";
+import { axiosRequest } from "../../api/axiosDefaults";
+
+import NoResults from "../NoResults";
+import PostCard from "./PostCard";
+import { AppContext } from "../../context/AppContext";
 
 const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+  const { user } = useContext(AppContext); 
+  const [posts, setPosts] = useState({ results: [] });
+  const [isLoaded, setIsLoaded] = useState(false);
 
-export default Feed
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axiosRequest.get("/posts/");
+        setPosts(data);
+        setIsLoaded(true);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-10 h-full gap-10 w-full">
+      {posts.results?.length ? (
+        posts.results.map((post) => (
+          <PostCard post={post} key={post.id} />
+        ))
+      ) : (
+        <NoResults message="No videos just yet :/" />
+      )}
+    </div>
+  );
+};
+
+export default Feed;
