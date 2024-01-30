24e307fe7a5e41c5e463d204a809d5373833df8e
Thiago Souza <mistersouza@icloud.com>
2024-01-30 18:54:26 +0000

    Write auth modal component

---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,71 @@
+import { useEffect, useRef, useState } from "react";
+import { ImCancelCircle } from "react-icons/im";
+import SignUpForm from "./auth/SignUpForm";
+import LogInForm from "./auth/LogInForm";
+
+const Modal = ({ modal, handleModalClick }) => {
+  const [isSignedUp, setIsSignedUp] = useState(true);
+  const modalRef = useRef();
+
+  const handleMousedown = ({ target }) => {
+    if (modalRef.current && !modalRef.current.contains(target))
+      handleModalClick();
+  };
+
+  useEffect(() => {
+    if (modal) {
+      document.addEventListener("mousedown", handleMousedown);
+    } else {
+      document.removeEventListener("mousedown", handleMousedown);
+    }
+
+    // Cleanup function
+    return () => {
+      document.removeEventListener("mousedown", handleMousedown);
+    };
+  }, [modal]);
+
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-60 backdrop-blur-sm z-9">
+      <div
+        className="flex flex-col gap-3 bg-white rounded shadow-lg w-1/5"
+        ref={modalRef}
+      >
+        <button className="self-end text-2xl p-3" onClick={handleModalClick}>
+          <ImCancelCircle />
+        </button>
+        <h2 className="flex-1 text-3xl text-center font-semibold">
+          {isSignedUp ? "Log in to ClipbHub" : "Sign up for ClipHub"}
+        </h2>
+        <div className="px-7">
+          {isSignedUp ? <LogInForm /> : <SignUpForm />}
+        </div>
+        <div className="text-xs text-gray-500 text-center p-2">
+          <p>
+            By continuing, you agree to ClipbHub's{" "}
+            <span className="text-gray-800 underline cursor-pointer">
+              Terms of Service
+            </span>{" "}
+            and confirm that you have read ClipbHub's{" "}
+            <span className="text-gray-800 underline cursor-pointer">
+              Privacy Policy
+            </span>
+          </p>
+        </div>
+        <div className="border-t border-gray-500 text-sm text-center py-5">
+          <p>
+            {isSignedUp ? "Don't have an account?" : "Already have an account?"}{" "}
+            <span
+              onClick={() => setIsSignedUp((prev) => !prev)}
+              className="text-gray-500 text-base cursor-pointer hover:underline hover:underline-offset-2"
+            >
+              {isSignedUp ? "Sign up" : "Log in"}
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
