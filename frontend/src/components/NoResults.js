55b6b007e9c66d37c9f10fe02412d5bc11bb4cea
Thiago Souza <mistersouza@icloud.com>
2024-02-03 18:16:55 +0000

    Style no results components

--- frontend/src/components/NoResults.js
+++ frontend/src/components/NoResults.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { MdOutlineVideocamOff } from 'react-icons/md'
+import { BiCommentX } from 'react-icons/bi'
 
-const NoResults = ({ message }) => {
+const NoResults = ({ message, noClips, noComments }) => {
   return (
-    <div className='text-center'>{message}</div>
+    <div className='flex flex-col justify-center items-center gap-1 text-sm w-full h-full'>
+      {noClips && <MdOutlineVideocamOff className='text-8xl' />}
+      {noComments && <BiCommentX className='text-7xl' />}
+      <p>{message}</p>
+    </div>
   )
 }
 
