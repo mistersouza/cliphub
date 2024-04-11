af630e64d71b5d2c6c296400384402600f373395
Thiago Souza <mistersouza@icloud.com>
2024-04-11 08:59:14 +0000

    Update categories

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,5 @@
-import { BsCode, BsEmojiSunglasses } from 'react-icons/bs';
-import { GiCakeSlice, GiGalaxy, GiLipstick } from 'react-icons/gi';
-import { FaPaw, FaMedal, FaGamepad } from 'react-icons/fa';
+import { BsCode } from 'react-icons/bs';
+import { FaShoppingBag, FaMusic, FaPaintBrush, FaCameraRetro, FaPlaneDeparture, FaPalette, FaUtensils } from 'react-icons/fa';
 
 export const topics = [
   {
@@ -8,34 +7,35 @@ export const topics = [
     icon: <BsCode />,
   },
   {
-    name: 'comedy',
-    icon: <BsEmojiSunglasses />,
+    name: 'fashion',
+    icon: <FaShoppingBag />,
   },
   {
-    name: 'gaming',
-    icon: <FaGamepad />,
+    name: 'music',
+    icon: <FaMusic />,
+  },
+  {
+    name: 'art',
+    icon: <FaPaintBrush />,
+  },
+  {
+    name: 'photography',
+    icon: <FaCameraRetro />,
+  },
+  {
+    name: 'travel',
+    icon: <FaPlaneDeparture />,
+  },
+  {
+    name: 'makeup',
+    icon: <FaPalette />,
   },
   {
     name: 'food',
-    icon: <GiCakeSlice />,
-  },
-  {
-    name: 'dance',
-    icon: <GiGalaxy />,
-  },
-  {
-    name: 'beauty',
-    icon: <GiLipstick />,
-  },
-  {
-    name: 'animals',
-    icon: <FaPaw />,
-  },
-  {
-    name: 'sports',
-    icon: <FaMedal />,
+    icon: <FaUtensils />,
   },
 ];
+
 
 export const aboutAndContactLinks = ['About', 'Newsroom', 'Store', 'Contact', 'Carrers', 'ByteDance', 'Creator Directory'];
 
