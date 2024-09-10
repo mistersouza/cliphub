2b0cdf84a7f378f347899a073a1302edc0d280e7
Thiago Souza <mistersouza@icloud.com>
2024-09-10 21:39:26 +0000

    Move Router component to index file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Posts/Feed';
@@ -14,31 +14,29 @@ const App = () => {
   const profileId = user?.profile_id || '';
 
   return (
-    <Router>
-      <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
-        <Navbar />
-        <div className="flex gap-6 md:gap-20 py-3">
-          <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto max-w-sm">
-            <Sidebar />
-          </div>
-          <div className="flex flex-col flex-1 gap-10 overflow-auto h-[88vh] videos">
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <Feed
-                    filter={`owner__followed__owner__profile=${profileId}&`}
-                  />
-                }
-              />
-              <Route path="/posts/:id" element={<ClipDetail />} />
-              <Route path="/upload" element={<ClipUpload />} />
-              <Route path="/profiles/:id" element={<UserProfile />} />
-            </Routes>
-          </div>
+    <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+      <Navbar />
+      <div className="flex gap-6 md:gap-20 py-3">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto max-w-sm">
+          <Sidebar />
+        </div>
+        <div className="flex flex-col flex-1 gap-10 overflow-auto h-[88vh] videos">
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Feed
+                  filter={`owner__followed__owner__profile=${profileId}&`}
+                />
+              }
+            />
+            <Route path="/posts/:id" element={<ClipDetail />} />
+            <Route path="/upload" element={<ClipUpload />} />
+            <Route path="/profiles/:id" element={<UserProfile />} />
+          </Routes>
         </div>
       </div>
-    </Router>
+    </div>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppProvider } from './context/AppContext';
+import { BrowserRouter as Router } from 'react-router-dom'; // Import Router here
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <Router>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </Router>
   </React.StrictMode>
 );
 
