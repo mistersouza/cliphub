b5d604e79ca6732759c9ab7cfad1c1cc09ce308f
Thiago Souza <mistersouza@icloud.com>
2024-09-12 13:49:52 +0000

    Reset baseURL

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'api/';
+axios.defaults.baseURL = 'https://8000-mistersouza-cliphub-0rgnlzr37ma.ws.codeinstitute-ide.net/api/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
