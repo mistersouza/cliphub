d7c9345ea160ba2f3f5d67125cd602ed38690db3
Thiago Souza <mistersouza@icloud.com>
2024-09-14 12:47:05 +0000

    Write custom hooks for modal and outside click detection

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,4 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+/**
+ * Custom hook to manage the state of an authentication modal.
+ * @returns {[boolean, Function]} An array containing:
+ * - `showAuthModal`: A boolean indicating whether the modal is visible.
+ * - `toggleAuthModalClick`: A function to toggle the modal's visibility.
+ */
 
 const useAuthModal = () => {
   const [showAuthModal, setShowAuthModal] = useState(false);
@@ -10,5 +17,29 @@ const useAuthModal = () => {
   return [showAuthModal, toggleAuthModalClick];
 };
 
-export { useAuthModal }; 
+/**
+ * Custom hook to detect clicks outside of the provided ref.
+ * @param {Function} handler 
+ * @returns {Object} The ref object 
+*/
+
+const useClickAway = (handler) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+      const handleMousedown = (event) => {
+          if (ref.current && !ref.current.contains(event.target)) handler();
+      };
+      document.addEventListener('mousedown', handleMousedown);
+      return () => {
+          document.removeEventListener('mousedown', handleMousedown);
+      };
+  }, [handler]);
+
+  return ref;
+}
+
+
+
+export { useAuthModal, useClickAway }; 
 
