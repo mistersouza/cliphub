1fd4dce1ff22cc259299cc1dbdb56e4204ab6665
Thiago Souza <mistersouza@icloud.com>
2024-09-15 22:22:40 +0000

    Add custom hook useDisplay

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,45 +1,42 @@
 import { useEffect, useRef, useState } from 'react';
 
 /**
- * Custom hook to manage the state of an authentication modal.
+ * Custom hook to manage the display state of any component.
+ * @param {boolean} [initialState=false] - The initial state.
  * @returns {[boolean, Function]} An array containing:
- * - `showAuthModal`: A boolean indicating whether the modal is visible.
- * - `toggleAuthModalClick`: A function to toggle the modal's visibility.
+ * - `isDisplayed`: A boolean indicating whether the component is displayed.
+ * - `toggleDisplay`: A function to toggle the component's display state.
  */
+const useDisplay = (initialState = false) => {
+  const [isDisplayed, setIsDisplayed] = useState(initialState);
 
-const useAuthModal = () => {
-  const [showAuthModal, setShowAuthModal] = useState(false);
-
-  const toggleAuthModalClick = () => {
-    setShowAuthModal((prev) => !prev);
+  const toggleDisplay = () => {
+    setIsDisplayed((prev) => !prev);
   };
 
-  return [showAuthModal, toggleAuthModalClick];
+  return [isDisplayed, toggleDisplay];
 };
 
 /**
  * Custom hook to detect clicks outside of the provided ref.
- * @param {Function} handler 
- * @returns {Object} The ref object 
-*/
+ * @param {Function} handler
+ * @returns {Object} The ref object
+ */
 
 const useClickAway = (handler) => {
   const ref = useRef(null);
 
   useEffect(() => {
-      const handleMousedown = (event) => {
-          if (ref.current && !ref.current.contains(event.target)) handler();
-      };
-      document.addEventListener('mousedown', handleMousedown);
-      return () => {
-          document.removeEventListener('mousedown', handleMousedown);
-      };
+    const handleMousedown = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) handler();
+    };
+    document.addEventListener('mousedown', handleMousedown);
+    return () => {
+      document.removeEventListener('mousedown', handleMousedown);
+    };
   }, [handler]);
 
   return ref;
-}
+};
 
-
-
-export { useAuthModal, useClickAway }; 
-
+export { useDisplay, useClickAway };
