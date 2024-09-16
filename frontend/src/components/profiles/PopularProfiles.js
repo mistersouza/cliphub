676b9013a649d0adbdd201d5d625496c759b20e8
Thiago Souza <mistersouza@icloud.com>
2024-09-16 21:00:06 +0000

    Refactor  use Avatar component

--- frontend/src/components/profiles/PopularProfiles.js
+++ frontend/src/components/profiles/PopularProfiles.js
@@ -1,14 +1,20 @@
 // React imports
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 // Dependacies imports
 import { Link } from 'react-router-dom';
 // Icons imports
 import { GoVerified } from 'react-icons/go';
 import { AppContext } from '../../context/AppContext';
+// Components import
+import Avatar from '../Avatar';
 
 const PopularProfiles = () => {
   const { profiles } = useContext(AppContext);
   const { popularProfiles } = profiles;
+
+  useEffect(() => {
+    console.log('profile detail', popularProfiles?.results);
+  });
 
   return (
     <div className="xl:border-b-2 border-gray-200 py-3">
@@ -17,13 +23,7 @@ const PopularProfiles = () => {
         {popularProfiles?.results.slice(0, 5).map((profile) => (
           <div className="flex items-center gap-2.5 px-1.5" key={profile.id}>
             <Link to={`/profiles/${profile.id}`}>
-              <div
-                className="
-                  size-10 bg-no-repeat bg-center bg-cover rounded-full 
-                  cursor-pointer
-                "
-                style={{ backgroundImage: `url(${profile.image})` }}
-              ></div>
+              <Avatar src={profile.image} />
             </Link>
             <div className="hidden xl:flex flex-col">
               <span
