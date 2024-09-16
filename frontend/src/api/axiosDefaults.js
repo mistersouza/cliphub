c3f968ec8b434008d6cfba6717544f23f054bb29
Thiago Souza <mistersouza@icloud.com>
2024-09-16 15:08:09 +0000

    Standardize code style and formatting

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://8000-mistersouza-cliphub-0rgnlzr37ma.ws.codeinstitute-ide.net/api/';
+axios.defaults.baseURL =
+  'https://8000-mistersouza-cliphub-0rgnlzr37ma.ws.codeinstitute-ide.net/api/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
