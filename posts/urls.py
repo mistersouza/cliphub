d5a7f1f2cc239ed11a8d3a8f9fa9e2a617c6d9ef
Thiago Souza <mistersouza@icloud.com>
2024-09-16 10:22:05 +0000

    Start 'clips' app and replace 'posts' app

--- posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('posts/', PostList.as_view(), name='clip-list'),
-    path('posts/<int:pk>/', PostDetail.as_view(), name='clip-detail'),
-    path('posts/<int:pk>/play/', PlayClipView.as_view(), name='clip-play'),
-]
