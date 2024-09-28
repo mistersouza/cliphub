86f48e0b2260f8fc769630ae3e47989b42298930
Thiago Souza <mistersouza@icloud.com>
2024-09-28 13:57:01 +0000

    Enhance compoent accessiblity

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -44,7 +44,7 @@ const Navbar = () => {
         flex justify-between items-center border-b-2 border-gray-200 py-1 px-3
       "
     >
-      <Link to="/">
+      <Link to="/" aria-label="Back to the homepage - ClipHub">
         <img
           className="w-16 md:w-20 lg:w-28 h-auto mx-auto"
           src="/cliphub_logo.png"
@@ -57,23 +57,28 @@ const Navbar = () => {
           className="absolute md:static top-10 -left-20 bg-white"
           onSubmit={() => {}}
         >
+          <label htmlFor="search-clips" className="sr-only">
+            Search clips
+          </label>
           <input
-            value={queryInput}
-            onChange={({ target }) => setQueryInput(target.value)}
             className="
-              bg-gray-50 p-2 md:text-md font-medium border-2 
-              border-gray-50 focus:outline-none
-              focus:border-gray-300 w-[300px] md:w-[350px] 
-              rounded-full md:top-0
+            bg-gray-50 p-2 md:text-md font-medium border-2 
+            border-gray-50 focus:outline-none
+            focus:border-gray-300 w-[300px] md:w-[350px] 
+            rounded-full md:top-0
             "
+            id="search-clips"
+            onChange={({ target }) => setQueryInput(target.value)}
+            value={queryInput}
             placeholder="Search clips"
           />
           <button
-            onClick={handleSearchSubmit}
             className="
-              absolute md:right-5 right-6 top-2.5 border-l-2 
-              border-gray-300 pl-4 text-2xl text-gray-400
+            absolute md:right-5 right-6 top-2.5 border-l-2 
+            border-gray-300 pl-4 text-2xl text-gray-400
             "
+            aria-label="Search"
+            onClick={handleSearchSubmit}
           >
             <BiSearch />
           </button>
@@ -81,18 +86,23 @@ const Navbar = () => {
       </div>
       {user ? (
         <div className="flex items-center gap-2 md:gap-5">
-          <Link to={'/upload'}>
+          <Link to={'/upload'} aria-label="Upload a new clip" role="button">
             <button
               className="
                 hidden md:flex items-center justify-between bg-transparent text-lg text-gray-800 
                 border border-gray-800 rounded hover:bg-gray-800 hover:text-gray-50
               "
+              aria-hidden="true"
             >
               <IoMdAdd className="text-xl" />
               <span className="hidden md:block text-lg pr-1">Upload</span>
             </button>
           </Link>
-          <button className="relative" onClick={toggleDropdownMenuDisplay}>
+          <button
+            className="relative"
+            onClick={toggleDropdownMenuDisplay}
+            aria-label="Your options"
+          >
             <Avatar src={user.profile_image} />
             <DropdownMenu
               menu={isDropdownMenuDisplayed}
@@ -113,7 +123,11 @@ const Navbar = () => {
           >
             Log in
           </button>
-          <button className="relative" onClick={toggleDropdownMenuDisplay}>
+          <button
+            className="relative"
+            onClick={toggleDropdownMenuDisplay}
+            aria-label="Log In"
+          >
             <PiDotsThreeVerticalBold className="text-2xl text-gray-700" />
             <DropdownMenu
               menu={isDropdownMenuDisplayed}
